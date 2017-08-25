9b7ddcc0bbf3af94c2b6950fbaaa9fc2456a8efb
ChrisCintron <Chriscintron502@gmail.com>
2017-08-24 19:35:37 -0500

    Add Gateway test

--- test_suite.py
+++ test_suite.py
@@ -6,7 +6,11 @@ def test_AllUsersInfo_get(): #All users
     results = api_backend.AllUsersInfo.get('self')
     assert type(results) == list
 
-
 def test_UserInfo_get(): #Single User
     results = api_backend.UserInfo.get('self', 'Chris')
+    assert type(results) == list
+
+def test_DB_Gateway_request():
+    query = "SELECT * FROM userinfo"
+    results = api_backend.DB_Gateway.request(query)
     assert type(results) == list
