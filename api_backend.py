b0575ad7de7e49ec73b0f13c9be336014ade9a51
ChrisCintron <Chriscintron502@gmail.com>
2017-08-24 20:53:26 -0500

    Add PUT and DELETE methods on ALLUsersInfo class

--- api_backend.py
+++ api_backend.py
@@ -1,5 +1,18 @@
 import sqlite3
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+
+#parser = reqparse.RequestParser()
+#parser.add_argument('username', type=int, help='The user username')
+#parser.add_argument('password', type=str, help="The password")
+#args = parser.parse_args()
+
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('username')
+parser.add_argument('password')
+parser.add_argument('userid')
 
 
 #Make test for these
@@ -19,15 +32,32 @@ class DB_Gateway:
         database = DB()
         database.c.execute(query)
         results = database.c.fetchall()
+        database.connection.commit()
         database.disconnect()
         return results
 
 class AllUsersInfo(Resource):
     def get(self):
-        query = """SELECT * FROM userinfo"""
+        query = "SELECT * FROM userinfo"
         mydata = DB_Gateway.request(query)
         results = [dict(ix) for ix in mydata]
         return results
+
+    def put(self):
+        args = parser.parse_args() #Args comes in {'username': "jeff", 'password': 'pass12'}
+        query = "INSERT INTO userinfo (user_name,user_password) values ('{}','{}');".format(args['username'],args['password'])
+        mydata = DB_Gateway.request(query)
+        results = "Success: User was PUT!"
+        return results
+
+    def delete(self):
+        args = parser.parse_args()
+        query = "DELETE FROM userinfo where user_id='{}';".format(args['userid'])
+        mydata = DB_Gateway.request(query)
+        results = "Success: User_id [{}] was DELETED!".format(args['userid'])
+        return results
+
+
 
 class UserInfo(Resource):
     def get(self,username):
